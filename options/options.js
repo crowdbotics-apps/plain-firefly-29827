64b6bfddb1963d048bbe05540bddc7f5a4e247b8
Crowdbotics <team@crowdbotics.com>
2021-08-15 17:21:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "plain_firefly_29827Identifier",
+  url: "https://plain_firefly_29827Identifier.botics.co",
+  api: "https://plain_firefly_29827Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
